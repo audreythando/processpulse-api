ac53fb4ce92edee3212e11ef08c0bf7391ac5698
audrey <audreym.sturdy@gmail.com>
2026-03-22 12:07:07 +0200

    chore: register WorkflowService in dependency injection

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
+using ProcessPulse.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<WorkflowService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
