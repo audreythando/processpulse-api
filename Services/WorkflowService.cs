a9fa46faad3b2acb434eaae276042e08ac830a48
audrey <audreym.sturdy@gmail.com>
2026-03-22 15:17:53 +0200

    feat: add CRUD methods to WorkflowService

--- Services/WorkflowService.cs
+++ Services/WorkflowService.cs
@@ -27,4 +27,52 @@ public class WorkflowService
     {
         return _workflows;
     }
+
+    public Workflow GetById(int id)
+    {
+        // return _workflows.FirstOrDefault(w => w.Id == id);
+        return _workflows[id - 1];
+    }
+
+    public Workflow Create(Workflow workflow)
+    {
+        var nextId = _workflows.Any() ?  _workflows.Max(w => w.Id) + 1 : 1;
+        workflow.Id = nextId;
+        workflow.CreatedAt = DateTime.Now;
+
+        _workflows.Add(workflow);
+
+        return workflow;
+    }
+
+
+    public bool Update(int id, Workflow workflow)
+    {
+        var existingWorkflow = _workflows.FirstOrDefault(w => w.Id == id);
+
+        if (existingWorkflow == null)
+        {
+            return false;
+        }
+
+        existingWorkflow.Name = workflow.Name;
+        existingWorkflow.Status = workflow.Status;
+        existingWorkflow.Owner = workflow.Owner;
+
+        return true;
+    }
+
+    public bool Delete(int id)
+    {
+        var workflow = _workflows.FirstOrDefault(w => w.Id == id);
+
+        if (workflow == null)
+        {
+            return false;
+        }
+
+        _workflows.Remove(workflow);
+
+        return true;
+    }
 }
